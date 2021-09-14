c2be3a80d2185c644e31ce110fee38989a0cafc5
zhu327 <zhu327@gmail.com>
2021-09-14 17:59:28 +0800

    fix: fix v3 smart

--- saas/config/prod.py
+++ saas/config/prod.py
@@ -47,7 +47,7 @@ if "BKPAAS_ENVIRONMENT" in os.environ:
     # 兼容component的APP_ID,APP_TOKEN
     APP_CODE = APP_ID = os.environ.get("BKPAAS_APP_ID", APP_CODE)
     SECRET_KEY = APP_TOKEN = os.environ.get("BKPAAS_APP_SECRET", SECRET_KEY)
-    BK_PAAS_INNER_HOST = os.environ.get("BK_PAAS2_INNER_URL", BK_PAAS_INNER_HOST)
+    BK_PAAS_INNER_HOST = os.environ.get("BK_PAAS2_URL", BK_PAAS_INNER_HOST)
     BK_COMPONENT_API_URL = os.environ.get("BK_COMPONENT_API_URL")
 
     # 正式环境数据库可以在这里配置
