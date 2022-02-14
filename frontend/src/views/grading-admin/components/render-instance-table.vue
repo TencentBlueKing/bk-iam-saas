b3095c177727ee0d8604357bb6ef45738eae76d9
polo <polohuang0428@gmail.com>
2022-02-14 16:57:22 +0800

    fix: add code

--- frontend/src/views/grading-admin/components/render-instance-table.vue
+++ frontend/src/views/grading-admin/components/render-instance-table.vue
@@ -427,8 +427,7 @@
                         && !item.isAggregate
                         && relatedActions.includes(item.id)
                         && curData.system_id === item.system_id
-                        && item.related_resource_types
-                        && !item.related_resource_types.every(sub => sub.empty)
+                        && !item.resource_groups[this.curGroupIndex].related_resource_types.every(sub => sub.empty)
                 }))
                 if (relatedList.length > 0) {
                     relatedList.forEach(item => {
