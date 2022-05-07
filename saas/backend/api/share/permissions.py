a01014edee8aee08f696c2291bea6f4f073f1b65
nannan00 <17491932+nannan00@users.noreply.github.com>
2022-05-07 14:55:54 +0800

    fix(share api): permission error (#1043)

--- saas/backend/api/share/permissions.py
+++ saas/backend/api/share/permissions.py
@@ -30,6 +30,6 @@ class ShareAPIPermission(permissions.IsAuthenticated):
         raise exceptions.PermissionDenied(detail=f"app_code({app_code}) do not be allowed to call share api")
 
     @staticmethod
-    def is_share_app_code(self, app_code):
+    def is_share_app_code(app_code):
         share_app_codes = set(settings.SHARE_APP_CODES.split(","))
         return app_code in share_app_codes
