acdd293d96189754ca798704b92118b89047a987
wklken <wklken@gmail.com>
2022-06-14 19:49:53 +0800

    refactor(remove): unuse import

--- saas/backend/api/admin/views/subject.py
+++ saas/backend/api/admin/views/subject.py
@@ -8,7 +8,6 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-from django.utils.translation import gettext as _
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.response import Response
@@ -20,9 +19,7 @@ from backend.api.admin.serializers import AdminSubjectGroupSLZ, SubjectRoleSLZ
 from backend.api.authentication import ESBAuthentication
 from backend.biz.group import GroupBiz
 from backend.biz.role import RoleBiz
-from backend.common.error_codes import error_codes
 from backend.common.pagination import CustomPageNumberPagination
-from backend.service.constants import SubjectType
 from backend.service.models import Subject
 
 
