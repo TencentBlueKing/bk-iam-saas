926bca05d71db17772076dd3390e6caa34cfc675
小铭 <3303710797@qq.com>
2022-07-21 15:37:54 +0800

    fix: 保存为常用操作 样式问题

--- frontend/src/components/common-action/index.vue
+++ frontend/src/components/common-action/index.vue
@@ -14,7 +14,7 @@
                 v-if="item.id !== 0 && isEditMode"
                 @click.stop="handleDelete(item.id, item.$id, index)" />
         </section>
-        <template v-if="isEditMode">
+        <template v-if="isEditMode && !isDisabled">
             <bk-button
                 text
                 theme="primary"
