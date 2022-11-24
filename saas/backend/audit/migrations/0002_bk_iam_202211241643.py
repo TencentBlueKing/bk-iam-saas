4dc2a238f5c870554fd423e94d473b8403eb85f9
zhu327 <zhu327@gmail.com>
2022-11-24 16:44:16 +0800

    feat: add audit migration file

---
+++ saas/backend/audit/migrations/0002_bk_iam_202211241643.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import codecs
+import json
+import os
+
+from django.conf import settings
+from django.db import migrations
+from iam.contrib.iam_migration.migrator import IAMMigrator
+
+
+def forward_func(apps, schema_editor):
+
+    migrator = IAMMigrator(Migration.migration_json)
+    migrator.migrate()
+
+
+class Migration(migrations.Migration):
+    migration_json = "01_initial.json"
+
+    dependencies = [('audit', '0001_initial')]
+
+    operations = [
+        migrations.RunPython(forward_func)
+    ]
