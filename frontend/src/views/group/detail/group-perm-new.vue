4a4af958269fd0d922d1b7e887aa9eea0147c1f4
逍遥尊者 <zly2576885332@163.com>
2022-12-13 16:35:57 +0800

    fix:用户组详情—>组权限展示问题->只有一条数据默认展开

--- frontend/src/views/group/detail/group-perm-new.vue
+++ frontend/src/views/group/detail/group-perm-new.vue
@@ -232,11 +232,11 @@
                     });
                 } finally {
                     groupSystem.loading = false;
-                    // if (res.data.length === 1) {
-                    //     this.$nextTick(() => {
-                    //         this.$refs[`rTemplateItem${groupSystem.id}`][0].handleExpanded();
-                    //     });
-                    // }
+                    if (res.data.length === 1) {
+                        this.$nextTick(() => {
+                            this.$refs[`rTemplateItem${groupSystem.id}`][0].handleExpanded();
+                        });
+                    }
                 }
             },
 
