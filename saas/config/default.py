ec128c35a68d88fe81a5eed53a23d86761548d49
zhu327 <zhu327@gmail.com>
2023-03-02 14:35:34 +0800

    fix: update metric default token

--- saas/config/default.py
+++ saas/config/default.py
@@ -426,4 +426,4 @@ BK_IAM_MIGRATION_JSON_PATH = "resources/iam/"
 
 
 # IAM metric 接口密码
-BK_IAM_METRIC_TOKEN = env.str("BK_IAM_METRIC_TOKEN", default="D@or!J4jNJ4LInub")
+BK_IAM_METRIC_TOKEN = env.str("BK_IAM_METRIC_TOKEN", default="")
