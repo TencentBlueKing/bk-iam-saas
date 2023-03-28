09ad8de86fe80c1f48817db1474b322636b01e4c
zhu327 <zhu327@gmail.com>
2023-03-28 15:34:08 +0800

    feat: list instance filter with action

--- saas/backend/service/resource.py
+++ saas/backend/service/resource.py
@@ -181,13 +181,20 @@ class ResourceProvider:
         return count, [ResourceAttributeValue(**i) for i in results]
 
     def list_instance(
-        self, ancestors: List[Dict[str, str]], limit: int = 10, offset: int = 0
+        self,
+        ancestors: List[Dict[str, str]],
+        limit: int = 10,
+        offset: int = 0,
+        action_system_id: str = "",
+        action_id: str = "",
     ) -> Tuple[int, List[ResourceInstanceBaseInfo]]:
         """根据上级资源获取某个资源实例列表"""
         filter_condition: Dict[str, Any] = {}
         if ancestors:
             filter_condition["ancestors"] = ancestors
             filter_condition["parent"] = {"type": ancestors[-1]["type"], "id": ancestors[-1]["id"]}
+        if action_system_id and action_id:
+            filter_condition["action"] = {"system": action_system_id, "id": action_id}
         page = self._get_page_params(limit, offset)
         count, results = self.client.list_instance(filter_condition, page)
 
@@ -200,13 +207,22 @@ class ResourceProvider:
         return count, instance_results
 
     def search_instance(
-        self, keyword: str, parent_type: str = "", parent_id: str = "", limit: int = 10, offset: int = 0
+        self,
+        keyword: str,
+        parent_type: str = "",
+        parent_id: str = "",
+        limit: int = 10,
+        offset: int = 0,
+        action_system_id: str = "",
+        action_id: str = "",
     ) -> Tuple[int, List[ResourceInstanceBaseInfo]]:
         """根据上级资源和Keyword搜索某个资源实例列表"""
         # Note: 虽然与list_instance很相似，但在制定回调接口协议时特意分开为两个API，这样方便后续搜索的扩展
         filter_condition: Dict = {"keyword": keyword}
         if parent_type and parent_id:
             filter_condition["parent"] = {"type": parent_type, "id": parent_id}
+        if action_system_id and action_id:
+            filter_condition["action"] = {"system": action_system_id, "id": action_id}
         page = self._get_page_params(limit, offset)
         count, results = self.client.search_instance(filter_condition, page)
 
