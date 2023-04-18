008bdc0e680c8bed177145a5e8af070b8cc32407
lhzzforever <zly2576885332@163.com>
2023-04-18 11:32:53 +0800

    fix: 修复无可批量复制策略资源资源实例时报错问题

--- frontend/src/views/manage-spaces/secondary-manage-space/components/render-condition.vue
+++ frontend/src/views/manage-spaces/secondary-manage-space/components/render-condition.vue
@@ -154,13 +154,17 @@
                     actions: this.params.actions.slice(0, 1)
                 };
                 try {
-                    const res = await this.$store.dispatch('permApply/resourceBatchCopy', params);
-                    console.warn(res);
-                    const condition = res.data[0].resource_type.condition;
-                    this.$emit('on-paste', {
-                        flag: true,
-                        data: condition
-                    });
+                    const { data } = await this.$store.dispatch('permApply/resourceBatchCopy', params);
+                    console.warn(data);
+                    if (data && data.length) {
+                        const condition = data[0].resource_type.condition;
+                        this.$emit('on-paste', {
+                            flag: true,
+                            data: condition
+                        });
+                    } else {
+                        this.messageError(this.$t(`m.info['暂无可批量复制的资源实例']`), 1000);
+                    }
                 } catch (e) {
                     this.$emit('on-paste', {
                         flag: false,
