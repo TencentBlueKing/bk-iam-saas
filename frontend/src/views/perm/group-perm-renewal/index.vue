fa36abf050ceaedf1d40bc551fdcf7d6026b22e4
lhzzforever <zly2576885332@163.com>
2023-04-19 18:19:34 +0800

    feature:  添加用户组成员续期多个批量续期列表数据默认选中

--- frontend/src/views/perm/group-perm-renewal/index.vue
+++ frontend/src/views/perm/group-perm-renewal/index.vue
@@ -168,6 +168,26 @@
         methods: {
             async fetchPageData () {
                 await this.fetchData();
+                await this.fetchDefaultCheck();
+            },
+
+            async fetchDefaultCheck () {
+                if (this.pagination.current === 1) {
+                    for (let i = 0; i < this.tableList.length; i++) {
+                        const item = this.tableList[i];
+                        this.$set(item, 'expanded', true);
+                        this.$set(item, 'checkList', []);
+                        await this.fetchMembers(item, i);
+                        if (item.children && item.children.length) {
+                            item.children.forEach(subItem => {
+                                this.currentSelectList.push(subItem);
+                                item.checkList.push(subItem);
+                                this.$refs.permTableRef
+                                    && this.$refs.permTableRef[i].toggleRowSelection(subItem, true);
+                            });
+                        }
+                    }
+                }
             },
 
             getIsSelect (item, index) {
@@ -262,13 +282,6 @@
                 }
             },
 
-            async handleExpanded (payload, item) {
-                item.pagination.current = 1;
-                item.pagination.limit = 10;
-                item.pagination.count = 0;
-                await this.fetchMembers(item);
-            },
-
             async fetchData (isLoading = false) {
                 this.tableLoading = isLoading;
                 try {
@@ -300,6 +313,13 @@
                 } finally {
                     this.tableLoading = false;
                 }
+            },
+
+            async handleExpanded (payload, item) {
+                item.pagination.current = 1;
+                item.pagination.limit = 10;
+                item.pagination.count = 0;
+                await this.fetchMembers(item);
             },
 
             handlePageChange (page) {
@@ -349,7 +369,6 @@
                 this.currentSelectList = _.cloneDeep(selection);
                 this.tableList[index].checkList = _.cloneDeep(selection);
                 this.setExpiredAt();
-                console.log(this.tableList[index].checkList);
             },
 
             handlerChange (selection, row, index) {
