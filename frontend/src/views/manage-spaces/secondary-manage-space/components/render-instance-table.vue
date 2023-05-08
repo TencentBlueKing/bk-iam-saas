055310a452e0678fe8d1795af0b6180c0ba48f6b
lhzzforever <zly2576885332@163.com>
2023-05-08 18:21:55 +0800

    fix: 授权边界展开栏上统计的操作权限数量应该是逐项编辑的总数量

--- frontend/src/views/manage-spaces/secondary-manage-space/components/render-instance-table.vue
+++ frontend/src/views/manage-spaces/secondary-manage-space/components/render-instance-table.vue
@@ -11,7 +11,7 @@
                         bk
                         :type="isExpandTable ? 'down-shape' : 'right-shape'" />
                     <span>{{ $t(`m.info['已添加']`) }}</span>
-                    <span class="number">{{ tableList.length }}</span>
+                    <span class="number">{{ totalCount }}</span>
                     <span>{{ $t(`m.common['个']`) }}{{ $t(`m.perm['操作权限']`) }}</span>
                 </div>
                 <div class="iam-resource-header-right">
@@ -332,6 +332,9 @@
             isGroup: {
                 type: Boolean,
                 default: false
+            },
+            totalCount: {
+                type: Number
             }
         },
         data () {
