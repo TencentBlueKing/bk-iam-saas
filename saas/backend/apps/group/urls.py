fa3b81ee757b792a522a1c6fb7a33ba18e97b58a
zhu327 <zhu327@gmail.com>
2023-05-19 15:36:20 +0800

    fix: group search url

--- saas/backend/apps/group/urls.py
+++ saas/backend/apps/group/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
         name="group.members",
     ),
     path("transfer/", views.GroupTransferView.as_view(), name="group.transfer"),
+    path("search/", views.GroupSearchViewSet.as_view({"post": "search"}), name="group.search"),
     # 用户组详情
     path(
         "<str:id>/",
@@ -72,5 +73,4 @@ urlpatterns = [
         views.GroupCustomPolicyConditionCompareView.as_view({"post": "create"}),
         name="group.custom_policy_condition_compare",
     ),
-    path("search/", views.GroupSearchViewSet.as_view({"post": "search"}), name="group.search"),
 ]
