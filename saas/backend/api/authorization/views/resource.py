92d582f28a2c40bccdc6e059527363e69a7c569f
Timmy <zhu327@gmail.com>
2023-05-25 17:40:42 +0800

    feat: mod itsm ticket use dynamic_fields (#2005)

--- saas/backend/api/authorization/views/resource.py
+++ saas/backend/api/authorization/views/resource.py
@@ -65,7 +65,7 @@ class AuthInstanceView(AuthViewMixin, APIView):
 
         audit_context_setter(operate=operate, subject=subject, system_id=system_id, policies=policies)
 
-        return self.policy_response(policies[0])
+        return self.policy_response(policies[0] if policies else {})
 
 
 class AuthPathView(AuthViewMixin, APIView):
@@ -109,7 +109,7 @@ class AuthPathView(AuthViewMixin, APIView):
 
         audit_context_setter(operate=operate, subject=subject, system_id=system_id, policies=policies)
 
-        return self.policy_response(policies[0])
+        return self.policy_response(policies[0] if policies else {})
 
 
 class AuthBatchInstanceView(AuthViewMixin, APIView):
