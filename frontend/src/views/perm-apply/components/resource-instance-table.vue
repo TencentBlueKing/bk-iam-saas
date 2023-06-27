c27573291ae0e42a046bd7423324f6193205c7c0
lhzzforever <zly2576885332@163.com>
2023-06-27 14:48:34 +0800

    fix:  修复申请自定义权限->资源实例表格无限制状态无法回显

--- frontend/src/views/perm-apply/components/resource-instance-table.vue
+++ frontend/src/views/perm-apply/components/resource-instance-table.vue
@@ -397,7 +397,8 @@
           if (!curData) {
               return [];
           }
-          if (curData.condition.length === 0) curData.condition = ['none'];
+          // 此处为空如果重新赋值，会一直走不到无限制业务代码逻辑
+          // if (curData.condition.length === 0) curData.condition = ['none'];
           return _.cloneDeep(curData.condition);
       },
       originalCondition () {
@@ -903,7 +904,7 @@
         if (relatedList.length > 0) {
           relatedList.forEach(item => {
             if (!item.policy_id) {
-              item.expired_at = item.expired_at + this.user.timestamp;
+              item.expired_at = Number(item.expired_at) + this.user.timestamp;
             }
             delete item.policy_id;
             item.resource_groups.forEach(groupItem => {
@@ -973,7 +974,7 @@
             const inOriginalList = !!this.originalList.filter(
               original => String(original.id) === String(item.id)
             ).length;
-            item.expired_at = item.expired_at - this.user.timestamp;
+            item.expired_at = Number(item.expired_at) - this.user.timestamp;
             this.tableList.splice(
               curIndex,
               1,
