b196342a60a0a5a0e4e6dabff9c418a23b2fec4d
lhzzforever <zly2576885332@163.com>
2023-09-04 19:26:00 +0800

    fix: 如果用户组成员为空，复制成员默认disabled

--- frontend/src/views/group/components/member-table.vue
+++ frontend/src/views/group/components/member-table.vue
@@ -19,7 +19,7 @@
             :list="COPY_KEYS_ENUM"
             :clearable="false"
             :ext-popover-cls="!curLanguageIsCn ? 'copy-user-group-cls copy-user-group-cls-lang' : 'copy-user-group-cls'"
-            :disabled="readOnly"
+            :disabled="isCopyDisabled"
             :placeholder="$t(`m.userGroup['复制成员']`)"
             :trigger="'hover'"
             :style="{ width: curLanguageIsCn ? '100px' : '140px' }"
@@ -285,14 +285,10 @@
               return this.getGroupAttributes && this.getGroupAttributes().source_from_role
               && this.pagination.count === 1;
           };
+      },
+      isCopyDisabled () {
+        return this.readOnly || !this.tableList.length;
       }
-    // formatCopyMembers () {
-    //   return this.currentSelectList.length
-    //   ? this.currentSelectList.map(v => v.type === 'user'
-    //    ? v.id
-    //    : (this.enableOrganizationCount ? `{${v.id}}${v.name}&full_name=${v.full_name}&count=${v.member_count}*`
-    //    : `{${v.id}}${v.name}&full_name=${v.full_name}*`)).join('\n') : '';
-    // }
     },
     watch: {
       'pagination.current' (value) {
