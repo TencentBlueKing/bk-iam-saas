d153297c7834091df00d4b833fa5215545542163
zhu327 <zhu327@gmail.com>
2024-01-15 15:32:21 +0800

    feat: enhancement of ITSM Ticket Content Display

--- saas/backend/plugins/application_ticket/itsm/ticket_content.py
+++ saas/backend/plugins/application_ticket/itsm/ticket_content.py
@@ -305,9 +305,48 @@ class ResourceGroupInfo(BaseModel):
 
     @classmethod
     def from_resource_groups(cls, resource_groups: ApplicationResourceGroupList) -> "ResourceGroupInfo":
-        value = [BaseDictStrValue(value=f"已设置 {len(resource_groups)} 个资源组合")]
+        value = [BaseDictStrValue(value=cls.gen_resource_summary(resource_groups))]
         children = [ResourceGroupTable.from_resource_groups(resource_groups)]
         return ResourceGroupInfo(value=value, children=children)
+
+    @staticmethod
+    def gen_resource_summary(resource_groups: ApplicationResourceGroupList) -> str:
+        """
+        生成资源组合概要信息
+        """
+        if len(resource_groups) != 1:
+            return f"已设置 {len(resource_groups)} 个资源组"
+
+        summary = []
+        for rg in resource_groups:
+            for rt in rg.related_resource_types:
+                resource_type_name = rt.name
+                if len(rt.condition) == 0:
+                    value = f"{resource_type_name}: 无限制"
+                else:
+                    # 解析资源条件
+                    resource_name = ""
+                    resource_count = 0
+                    attribute_count = 0
+                    for c in rt.condition:
+                        for instance in c.instances:
+                            resource_count += len(instance.path)
+                            if resource_name == "":
+                                resource_name = instance.path[0].name
+                        attribute_count += len(c.attributes)
+
+                    if attribute_count == 0 and resource_count == 1:
+                        value = f"{resource_name}1个{resource_type_name}"
+                    elif attribute_count == 0 and resource_count > 1:
+                        value = f"{resource_name}等{resource_count}个{resource_type_name}"
+                    elif attribute_count > 0 and resource_count == 0:
+                        value = f"{resource_type_name}: {attribute_count}个属性"
+                    else:
+                        value = f"{resource_count}个{resource_type_name}({attribute_count}个属性)"
+
+                summary.append(value)
+
+        return ", ".join(summary)
 
 
 # ---------------------------- 自定义权限申请 ----------------------------
@@ -393,6 +432,7 @@ class GroupColumnValue(BaseModel):
     desc: BaseDictStrValue
     expired_display: BaseDictStrValue
     group_info: GroupInfo
+    role_name = BaseDictStrValue
     highest_sensitivity_level: BaseDictStrValue  # 最高敏感等级
 
     @classmethod
@@ -402,6 +442,7 @@ class GroupColumnValue(BaseModel):
             desc=BaseDictStrValue(value=group.description),
             expired_display=BaseDictStrValue(value=group.expired_display),
             group_info=GroupInfo.from_group(group),
+            role_name=BaseDictStrValue(value=group.role_name),
             highest_sensitivity_level=BaseDictStrValue(
                 value=SensitivityLevel.get_choice_label(group.highest_sensitivity_level)
             ),
