fe06807beb0e480562e5062a8bc49eaaa6808099
ielgnaw <wuji0223@126.com>
2024-05-11 17:16:11 +0800

    feat: window.open 登录 (#2658)

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -214,7 +214,8 @@ function handleReject (error, config) {
       // nextError.message = error.response.data.message;
       // bus.$emit('show-login-modal', loginPlainUrl);
       // window.location = LOGIN_SERVICE_URL + '/?c_url=' + window.location.href
-      const loginCallbackURL = `${window.location.origin}/static/login_success.html?is_ajax=1`;
+      // const loginCallbackURL = `${window.location.origin}/static/login_success.html?is_ajax=1`;
+      const loginCallbackURL = `${window.origin + window.SITE_URL}login_success/?is_ajax=1`;
       const siteLoginUrl = window.LOGIN_SERVICE_URL || '';
       if (!siteLoginUrl) {
         console.error('Login URL not configured!');
