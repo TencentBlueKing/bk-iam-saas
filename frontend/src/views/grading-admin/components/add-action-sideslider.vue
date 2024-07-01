962e42a1442c3cbe09ede65a26d4e5e0e955a668
lhzzforever <zly2576885332@163.com>
2024-07-01 12:40:08 +0800

    fix: 添加自定义权限-查看已经选择的数据时，已选择的操作个数会消失 fix: 用户组名称限制emoj字符

--- frontend/src/views/grading-admin/components/add-action-sideslider.vue
+++ frontend/src/views/grading-admin/components/add-action-sideslider.vue
@@ -612,67 +612,60 @@
       },
 
       handleDefaultData (payload, data) {
-        this.tagActionListBackUp = [];
-        this.systemData[payload].list = _.cloneDeep(data);
-        this.systemData[payload].list.forEach(item => {
-          if (!item.actions) {
-            item.actions = [];
-          }
-          if (!item.sub_groups) {
-            item.sub_groups = [];
-          }
-          let allChecked = true;
-          // item.actions = item.actions.filter(v => !v.hidden);
-          item.actions.forEach(act => {
-            act.$id = `${payload}&${act.id}`;
-            act.related_resource_types.forEach(v => {
-              v.type = v.id;
-            });
-            this.$set(act, 'checked', this.defaultValue.includes(act.$id) || this.curSelectValue.includes(act.$id));
-            if (!act.checked) {
-              allChecked = false;
+        if (this.systemData[payload]) {
+          this.tagActionListBackUp = [];
+          this.$set(this.systemData[payload], 'count', 0);
+          this.$set(this.systemData[payload], 'list', _.cloneDeep(data));
+          this.systemData[payload].list.forEach((item) => {
+            if (!item.actions) {
+              item.actions = [];
             }
-            if (act.checked) {
-              this.tagActionListBackUp.push(act.id);
+            if (!item.sub_groups) {
+              item.sub_groups = [];
             }
-            this.linearAction.push(act);
-          });
-          item.sub_groups.forEach(act => {
-            // act.actions = act.actions.filter(v => !v.hidden);
-            (act.actions || []).forEach(v => {
-              v.$id = `${payload}&${v.id}`;
-              v.related_resource_types.forEach(subItem => {
-                subItem.type = subItem.id;
+            let allChecked = true;
+            // item.actions = item.actions.filter(v => !v.hidden);
+            item.actions.forEach(act => {
+              act.$id = `${payload}&${act.id}`;
+              act.related_resource_types.forEach(v => {
+                v.type = v.id;
               });
-              this.$set(v, 'checked', this.defaultValue.includes(v.$id) || this.curSelectValue.includes(v.$id));
-              if (!v.checked) {
+              this.$set(act, 'checked', this.defaultValue.includes(act.$id) || this.curSelectValue.includes(act.$id));
+              if (!act.checked) {
                 allChecked = false;
               }
-
-              if (v.checked) {
-                this.tagActionListBackUp.push(v.id);
+              if (act.checked) {
+                this.tagActionListBackUp.push(act.id);
               }
-              this.linearAction.push(v);
+              this.linearAction.push(act);
             });
-          });
-          this.$set(item, 'text', allChecked ? this.$t(`m.common['取消全选']`) : this.$t(`m.common['全选']`));
-        });
-        this.systemData[payload].system_name = this.systemList.find(item => item.id === payload).name;
-
-        if (this.defaultValue.length > 0) {
-          const curAllActionIds = [];
-          this.systemData[payload].list.forEach(item => {
-            item.actions.forEach(act => {
-              curAllActionIds.push(act.$id);
-            });
-            item.sub_groups.forEach(sub => {
-              (sub.actions || []).forEach(v => {
-                curAllActionIds.push(v.$id);
+            item.sub_groups.forEach(act => {
+              // act.actions = act.actions.filter(v => !v.hidden);
+              (act.actions || []).forEach(v => {
+                v.$id = `${payload}&${v.id}`;
+                v.related_resource_types.forEach(subItem => {
+                  subItem.type = subItem.id;
+                });
+                this.$set(v, 'checked', this.defaultValue.includes(v.$id) || this.curSelectValue.includes(v.$id));
+                if (!v.checked) {
+                  allChecked = false;
+                }
+                if (v.checked) {
+                  this.tagActionListBackUp.push(v.id);
+                }
+                this.linearAction.push(v);
               });
             });
+            this.$set(item, 'text', allChecked ? this.$t(`m.common['取消全选']`) : this.$t(`m.common['全选']`));
           });
-          const intersection = curAllActionIds.filter(item => this.defaultValue.includes(item));
-          this.systemData[payload].count = this.tagActionListBackUp.length || intersection.length;
+          const curSystem = this.systemList.find((item) => item.id === payload);
+          if (curSystem) {
+            const hasSelectedActions = this.linearAction.filter((v) => v.checked);
+            this.systemData[payload] = Object.assign(this.systemData[payload], {
+              system_name: curSystem.name,
+              count: hasSelectedActions.length
+            });
+          }
         }
       },
 
