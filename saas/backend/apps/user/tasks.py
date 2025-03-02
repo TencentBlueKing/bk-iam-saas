980c4171432fa24ee8ab2595d60440efce234704
bevis-canway <bevis-canway@canway.net>
2025-03-02 22:30:43 +0800

    feat：组件升级，修复celery组件升级问题 app.task.register() ==> app.register_task()

--- saas/backend/apps/user/tasks.py
+++ saas/backend/apps/user/tasks.py
@@ -147,7 +147,8 @@ class SendUserExpireRemindMailTask(Task):
                 logger.exception("send user_group_policy_expire_remind rtx fail, username=%s", user.username)
 
 
-current_app.tasks.register(SendUserExpireRemindMailTask())
+current_app.register_task(SendUserExpireRemindMailTask())
+
 
 
 @shared_task(ignore_result=True)
