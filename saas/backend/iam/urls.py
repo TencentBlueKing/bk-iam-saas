bcb6a25fd576f4666aa1408673c2ce331678862f
nannan00 <17491932+nannan00@users.noreply.github.com>
2025-05-28 22:30:10 +0800

    fix: role authentication must occur after login authentication (#2891)
    * fix: role authentication must occur after login authentication
    
    * docs: update 1.10.44 and 1.10.43 version log

--- saas/backend/iam/urls.py
+++ saas/backend/iam/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-权限中心(BlueKing-IAM) available.
+TencentBlueKing is pleased to support the open source community by making 蓝鲸智云 - 权限中心 (BlueKing-IAM) available.
 Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
 Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
 You may obtain a copy of the License at http://opensource.org/licenses/MIT
@@ -10,9 +10,8 @@ specific language governing permissions and limitations under the License.
 """
 from django.urls import path
 
-from backend.common.views import login_exempt
 from backend.iam.dispatchers import dispatcher
 
 urlpatterns = [
-    path("resources/", dispatcher.as_view([login_exempt])),
+    path("resources/", dispatcher.as_view()),
 ]
