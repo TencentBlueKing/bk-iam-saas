07e10b81e8fa7300599e67ee2b1591c93cbd308f
nannan00 <17491932+nannan00@users.noreply.github.com>
2025-08-21 19:53:28 +0800

    fix: tiny

--- frontend/build/webpack.base.conf.js
+++ frontend/build/webpack.base.conf.js
@@ -138,7 +138,13 @@ export default {
         to: resolve('dist/static/images'),
         toType: 'dir'
       }
-    ])
+    ]),
+    new webpack.DefinePlugin({
+      '__VUE_OPTIONS_API__': JSON.stringify(true),
+      '__VUE_PROD_DEVTOOLS__': JSON.stringify(false),
+      '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': JSON.stringify(false)
+
+    })
   ],
 
   performance: {
