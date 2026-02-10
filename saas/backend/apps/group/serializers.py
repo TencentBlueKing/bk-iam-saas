08f542734694de9c66a5172dd5b3ee00a4bd89a1
yunlongJia <strongyunlong@gmail.com>
2026-02-10 16:05:40 +0800

    fix:提交加入或续期申请时过期时间突破前端限制 # Reviewed, transaction id: 74093

--- saas/backend/apps/group/serializers.py
+++ saas/backend/apps/group/serializers.py
@@ -289,7 +289,7 @@ class GroupTransferSLZ(serializers.Serializer):
 class GroupMemberExpiredAtSLZ(GroupMemberSLZ, ExpiredAtSLZ):
     def validate_expired_at(self, value):
         # 过期时间不能大于一年
-        if value > DEFAULT_EXPIRED_DURATION:
+        if value > int(time.time()) + DEFAULT_EXPIRED_DURATION:
             raise serializers.ValidationError("The expiration date must not exceed one year.")
         return value
 
