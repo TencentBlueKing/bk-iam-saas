e8404b973a62c14406557e042ecb4c467dfd468c
yunlongJia <strongyunlong@gmail.com>
2026-04-08 15:44:44 +0800

    fix:修改sync_organization参数 # Reviewed, transaction id: 77891

--- saas/backend/apps/organization/tasks.py
+++ saas/backend/apps/organization/tasks.py
@@ -42,8 +42,8 @@ logger = logging.getLogger("celery")
 
 
 @shared_task(ignore_result=True)
-def sync_organization(tenant_id: str = "default", executor: str = SYNC_TASK_DEFAULT_EXECUTOR):
-    if tenant_id == "default":
+def sync_organization(tenant_id: str = "", executor: str = SYNC_TASK_DEFAULT_EXECUTOR):
+    if tenant_id:
         for tenant in BkUserClient(settings.BK_APP_TENANT_ID).list_tenant():
             _sync_organization(tenant["id"])
     else:
